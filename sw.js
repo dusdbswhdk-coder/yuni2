f612775bb08f2e4bfab83a39418c48d3d723874d
dusdbswhdk-coder <dusdbswhdk@gmail.com>
2026-09-22 18:52:57 +0900

    Refresh cache for 5-card set update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="cardnews-studio-v16-set5a";
-const ASSETS=["./","./index.html","./app.css?v=20260922-set5a","./app.js?v=20260922-set5a","./logo-bee.png?v=2","./manifest.webmanifest"];
+const CACHE="cardnews-studio-v17-set5b";
+const ASSETS=["./","./index.html","./app.css?v=20260922-set5a","./app.js?v=20260922-set5b","./logo-bee.png?v=2","./manifest.webmanifest"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request,{cache:"no-store"}).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request)));});
