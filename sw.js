a7748d2d5c4c2e023ebed3e5d9d58cfd1018a20e
dusdbswhdk-coder <dusdbswhdk@gmail.com>
2026-09-22 17:47:12 +0900

    Use simple logo-bee asset path

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="cardnews-studio-v13-logo3";
-const ASSETS=["./","./index.html","./app.css?v=20260922-logo3","./app.js?v=20260922-templatefix2","./bee-mascot-web.png?v=logo3","./manifest.webmanifest"];
+const CACHE="cardnews-studio-v14-logobee";
+const ASSETS=["./","./index.html","./app.css?v=20260922-logo3","./app.js?v=20260922-templatefix2","./logo-bee.png?v=1","./manifest.webmanifest"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request,{cache:"no-store"}).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request)));});
