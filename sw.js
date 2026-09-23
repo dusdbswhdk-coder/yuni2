763afcdcdb4d539512e5d30ce96594816b1fdb57
dusdbswhdk-coder <dusdbswhdk@gmail.com>
2026-09-23 12:01:05 +0900

    Refresh cache for card navigator layout

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="cardnews-studio-v21-report1";
-const ASSETS=["./","./index.html","./app.css?v=20260923-report1","./app.js?v=20260923-report1","./spellcheck.js?v=20260923-spell2","./logo-bee.png?v=2","./manifest.webmanifest"];
+const CACHE="cardnews-studio-v22-stripup1";
+const ASSETS=["./","./index.html","./app.css?v=20260923-stripup1","./app.js?v=20260923-report1","./spellcheck.js?v=20260923-spell2","./logo-bee.png?v=2","./manifest.webmanifest"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request,{cache:"no-store"}).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request)));});
